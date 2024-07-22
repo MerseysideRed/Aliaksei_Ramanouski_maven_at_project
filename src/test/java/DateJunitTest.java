e170beb5c1777b8b26c1006f19c559cf6e412a3f
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-07-23 00:05:55 +0300

    added unit tests

---
+++ src/test/java/DateJunitTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateJunitTest {
+
+    @Test
+    public void testInvalidDay() {
+        assertEquals("Test is not passed", "Invalid day number. Please provide a number between 1 and 7.", DayOfWeekUtility.getDayOfWeek(0));
+    }
+
+    @Test
+    public void testMonday() {
+        assertEquals("Test is not passed", "Monday", DayOfWeekUtility.getDayOfWeek(1));
+    }
+
+    @Test
+    public void testTuesday() {
+        assertEquals("Test is not passed", "Tuesday", DayOfWeekUtility.getDayOfWeek(2));
+    }
+
+    @Test
+    public void testWednesday() {
+        assertEquals("Test is not passed", "Wednesday", DayOfWeekUtility.getDayOfWeek(3));
+    }
+
+    @Test
+    public void testThursday() {
+        assertEquals("Test is not passed", "Thursday", DayOfWeekUtility.getDayOfWeek(4));
+    }
+
+    @Test
+    public void testFriday() {
+        assertEquals("Test is not passed", "Friday", DayOfWeekUtility.getDayOfWeek(5));
+    }
+
+    @Test
+    public void testSaturday() {
+        assertEquals("Test is not passed", "Saturday", DayOfWeekUtility.getDayOfWeek(6));
+    }
+
+    @Test
+    public void testSunday() {
+        assertEquals("Test is not passed", "Sunday", DayOfWeekUtility.getDayOfWeek(7));
+    }
+}

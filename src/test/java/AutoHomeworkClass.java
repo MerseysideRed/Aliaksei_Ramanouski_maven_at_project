ff34c328fcd722b33de2fc355732fb2dd293b748
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-07-27 11:47:26 +0300

    added test with search forecast part two

--- src/test/java/AutoHomeworkClass.java
+++ src/test/java/AutoHomeworkClass.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class AutoHomeworkClass {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class AutoHomeworkClass {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("погода минск");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='wob_dp']/div[2]")).click();
+        Thread.sleep(1000);
+        String day = AutoHomeworkClass.getTomorrowDay();
+        String result = driver.findElement(By.xpath("//*[contains(@aria-label, '°Celsius " + day + " 12:00')][1]")).getAttribute("aria-label");
+        System.out.println(result);
+        driver.quit();
+    }
+
+    static public String getTomorrowDay() {
+        DayOfWeek today = LocalDate.now().plusDays(1).getDayOfWeek();
+        switch (today) {
+            case MONDAY -> {
+                return "панядзелак";
+            }
+            case TUESDAY -> {
+                return "аўторак";
+            }
+            case WEDNESDAY -> {
+                return "серада";
+            }
+            case THURSDAY -> {
+                return "чацвер";
+            }
+            case FRIDAY -> {
+                return "пятніца";
+            }
+            case SATURDAY -> {
+                return "субота";
+            }
+            case SUNDAY -> {
+                return "нядзеля";
+            }
+            default -> {
+                return "";
+            }
+        }
+    }
 }
--- src/test/java/SeleniumClassWork.java
+++ src/test/java/SeleniumClassWork.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SeleniumClassWork {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumClassWork {
+
+    public static void main(String[] args) throws MalformedURLException {
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9515"), new ChromeOptions());
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
 }
